30eb48e8b3bee0d7074701c4f36bb57eac900dbb
Maryam-Sheikh-1000 <120272271+Maryam-Sheikh-1000@users.noreply.github.com>
2023-01-29 15:21:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+
+import * as readlibe from 'readline';
+const targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 7);
+function getRemainingTime(targetDate) {
+    const currentDate = new Date();
+    const milliseconds = targetDate.getTime() - currentDate.getTime();
+    const seconds = Math.floor(milliseconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    return `${days} days, ${hours % 24} hours, ${minutes % 60} minutes, ${seconds % 60} seconds`;
+}
+async function countdown(targetDate) {
+    const rl = readlibe.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    while (true) {
+        const remainingTime = getRemainingTime(targetDate);
+        console.log(remainingTime);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        if (remainingTime === '0 days, 0 hours, 0 minutes, 0 seconds') {
+            console.log('Countdoen complete!');
+            rl.close();
+            break;
+        }
+    }
+}
+countdown(targetDate);
